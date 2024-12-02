a93ac6f1f3365589f1cc938eee3425457775d17e
Pawel Ryndak <prynd@cae.com>
2024-12-02 14:10:33 +0100

    Add solution to 2015-12-02

---
+++ 2015/02/day_02_1.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import List, Tuple
+
+Present = Tuple[int, int, int]
+
+def read_presents(input_path: Path) -> List[Present]:
+    presents = []
+    with open(input_path, 'r') as file:
+        for line in file:
+            l, w, h = line.split("x")
+            presents.append((int(l), int(w), int(h)))
+    return presents
+
+def calculate_paper_requirement(present: Present) -> int:
+    l, w, h = present
+    return 2 * l * w + 2 * h * w + 2 * h * l + min(l * w, w * h, h * l)
+
+def main():
+    presents = read_presents(Path("input.txt"))
+    total_paper_needed = sum([calculate_paper_requirement(present) for present in presents])
+    print(total_paper_needed)
+
+if __name__ == '__main__':
+    main()
---
+++ 2015/02/day_02_2.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from typing import Tuple
+
+from day_02_1 import read_presents, Present
+
+
+def calculate_ribbon_requirement(present: Present) -> int:
+    side_requirement = calculate_side_requirement(present)
+    bow_requirement = calculate_cubic_volume(present)
+    return side_requirement + bow_requirement
+
+
+def calculate_side_requirement(present: Present) -> int:
+    a, b = find_two_smallest_dimensions(present)
+    return calculate_perimeter(a, b)
+
+
+def calculate_cubic_volume(present: Present) -> int:
+    l, w, h = present
+    return l * w * h
+
+
+def find_two_smallest_dimensions(present: Present) -> Tuple[int, int]:
+    l, w, h = present
+    sorted_dimensions = sorted([l, w, h])
+    return sorted_dimensions[0], sorted_dimensions[1]
+
+
+def calculate_perimeter(a: int, b: int) -> int:
+    return 2 * a + 2 * b
+
+
+def main():
+    presents = read_presents(Path("input.txt"))
+    total_ribbon_requirement = sum([calculate_ribbon_requirement(present) for present in presents])
+    print(total_ribbon_requirement)
+
+
+if __name__ == '__main__':
+    main()

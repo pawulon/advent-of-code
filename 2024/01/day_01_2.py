66c053be45c7e7ad247288304e891efc1409207f
Pawel Ryndak <prynd@cae.com>
2024-12-02 06:12:27 +0100

    Refactor 2024-12-01_2

--- 2024/01/day_01_2.py
+++ 2024/01/day_01_2.py
@@ -1,14 +1,10 @@
-from collections import Counter
 from pathlib import Path
 from day_01_1 import read_lists
 
 
-
-
 def main() -> None:
     left_list, right_list = read_lists(Path("input.txt"))
-    right_list_locations_counter = Counter(right_list)
-    similarity_score = sum([left_list[i] * right_list_locations_counter[left_list[i]]  for i in range(len(left_list))])
+    similarity_score = sum([index * right_list.count(index) for index in left_list])
     print(similarity_score)
 
 
